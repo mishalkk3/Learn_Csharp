a049d5f4c9059982135aee7c250f3103ff1b1bfb
mishalkk3 <mishal.kk@simelabs.com>
2021-12-01 12:51:21 +0530

    conditionals and iterations

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if - else
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon");
+            }
+            else
+            {
+                Console.WriteLine("It's night");
+            }
+
+            // Conditional op
+
+            bool isGoldCustomer = true;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
+
+            //Switch case
+
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Spring:
+                    Console.WriteLine();
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Go to Beach");
+                    break;
+                case Season.Autumn:
+                    Console.WriteLine("Beautiful Season");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("Be Home");
+                    break ;
+               default:
+                    Console.WriteLine("What season is it?");
+                    break;
+            }
+        }
+    }
+}
---
+++ Itrations/Itrations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itrations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //For loop
+            for(int i=1;i<=10;i++)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //For each loop
+            string name = "John Smith";
+
+            foreach(char s in name)
+            {
+                Console.WriteLine(s);
+            }
+
+            //While loop
+            while(true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                Console.WriteLine("echo "+input);
+            }
+
+            //Random Class
+            var random = new Random();
+            char[] buffer = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                buffer[i] = (char)('a'+random.Next(0,26));
+            }
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
